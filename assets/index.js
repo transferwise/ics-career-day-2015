5edfd86d8d94e92a148d36506f03d161c053a64c
Uku Pattak <uku.pattak@transferwise.com>
2015-03-19 15:08:34 +0200

    Fix post

--- assets/index.js
+++ assets/index.js
@@ -2,10 +2,16 @@ var submitted = false;
 
 function submitForm() {
   submitted = true;
-
-  setTimeout(function () {
-    $('form').slideUp(800, function () {
-      $('#wrapper').append('<h4 class="feedback">Thank you!</h4>');
-    });
-  }, 1000);
+  toggleForm();
 }
+
+function toggleForm() {
+  if (window.location.hash == '#thank-you')
+    $('#wrapper').append('<h4 class="feedback">Thank you!</h4>');
+  else
+    $('form').show();
+}
+
+$(function() {
+  toggleForm();
+});
