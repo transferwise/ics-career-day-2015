c8a362874e48074c06fa837f924bded85c97a17d
Uku Pattak <uku.pattak@transferwise.com>
2015-03-19 15:23:50 +0200

    Remove effects

--- assets/index.js
+++ assets/index.js
@@ -6,9 +6,8 @@ function submitForm() {
 
 function toggleForm() {
   if (window.location.hash == '#thank-you') {
-    $('form').slideUp(800, function () {
-      $('#wrapper').append('<h4 class="feedback">Thank you!</h4>');
-    });
+    $('form').hide();
+    $('#wrapper').append('<h4 class="feedback">Thank you!</h4>');
   } else {
     $('form').show();
   }
